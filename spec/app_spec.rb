bee787c6c66d6c571b40db4bf85514d2c0bc7aab
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-11-27 17:35:21 +0300

    Implemented finding a specific app by id

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,12 +7,23 @@ describe 'App' do
 
   it 'should find all' do
     response_file = File.new(Dir.getwd + '/spec/app_all_stub_response.txt')
-    stub_request(:get, 'https://fabric.io/api/v2/apps').to_return(:body => response_file, :status => 200)
+    stub_request(:get, /apps/).to_return(:body => response_file, :status => 200)
     session = Fabricio::Authorization::Session.new({
                                                        'access_token' => 'token',
                                                        'refresh_token' => 'token'
                                                    })
     result = Fabricio::App.all(session)
     expect(result.count).to equal(2)
+  end
+
+  it 'should find by id' do
+    response_file = File.new(Dir.getwd + '/spec/app_find_stub_response.txt')
+    stub_request(:get, /apps/).to_return(:body => response_file, :status => 200)
+    session = Fabricio::Authorization::Session.new({
+                                                       'access_token' => 'token',
+                                                       'refresh_token' => 'token'
+                                                   })
+    result = Fabricio::App.find('test_id', session)
+    expect(result).not_to be_nil
   end
 end
