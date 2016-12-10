b8cf87daee0caee16613927915647648fec4f390
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-12-10 14:41:31 +0300

    Added organization_id as a field of Session object

--- lib/fabricio/authorization/authorization_client.rb
+++ lib/fabricio/authorization/authorization_client.rb
@@ -2,8 +2,10 @@ require 'faraday'
 require 'json'
 require 'fabricio/authorization/session'
 require 'fabricio/authorization/abstract_session_storage'
+require 'fabricio/services/organization_service'
 
 AUTH_API_URL = 'https://instant.fabric.io/oauth/token'
+ORGANIZATION_API_URL = 'https://fabric.io/api/v2/organizations'
 
 module Fabricio
   module Authorization
@@ -25,9 +27,14 @@ module Fabricio
       private
 
       def perform_authorization(username, password, client_id, client_secret)
+        auth_data = obtain_auth_data(username, password, client_id, client_secret)
+        organization_id = obtain_organization_id(auth_data)
+        Session.new(auth_data, organization_id)
+      end
+
+      def obtain_auth_data(username, password, client_id, client_secret)
         conn = Faraday.new(:url => AUTH_API_URL) do |faraday|
-          faraday.response :logger                  # log requests to STDOUT
-          faraday.adapter Faraday.default_adapter  # make requests with Net::HTTP
+          faraday.adapter Faraday.default_adapter
         end
 
         response = conn.post do |req|
@@ -41,8 +48,18 @@ module Fabricio
               'client_secret' => client_secret
           }.to_json
         end
+        JSON.parse(response.body)
+      end
 
-        Session.new(JSON.parse(response.body))
+      def obtain_organization_id(auth_data)
+        conn = Faraday.new(:url => ORGANIZATION_API_URL) do |faraday|
+          faraday.adapter Faraday.default_adapter
+        end
+
+        response = conn.get do |req|
+          req.headers['Authorization'] = "Bearer #{auth_data['access_token']}"
+        end
+        JSON.parse(response.body).first['id']
       end
     end
   end
