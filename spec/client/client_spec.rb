80685b97da04fc120a86b57c7a41714eb843a2d0
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-12-10 13:53:00 +0300

    Added missing tests for client.rb

--- spec/client/client_spec.rb
+++ spec/client/client_spec.rb
@@ -1,17 +1,40 @@
 require 'rspec'
-require 'fabricio/configuration/configuration'
 require 'fabricio/client/client'
+require 'fabricio/authorization/memory_session_storage'
+require 'fabricio/authorization/session'
 
 describe 'Client' do
 
+  before(:each) do
+    @storage = Fabricio::Authorization::MemorySessionStorage.new
+    @storage.store_session(Fabricio::Authorization::Session.new({}))
+  end
+
   it 'should be configurable' do
     test_id = '123'
-    client = Fabricio::Client.new
-    client.configure do |config|
+    client = Fabricio::Client.new do |config|
       config.client_id = test_id
+      config.session_storage = @storage
     end
 
     expect(client.client_id).to eq test_id
   end
 
+  it 'should return existing service' do
+    client = Fabricio::Client.new do |config|
+      config.session_storage = @storage
+    end
+    service = client.organization
+    expect(service).not_to be_nil
+  end
+
+  it 'should throw exception for non-existing service' do
+    client = Fabricio::Client.new do |config|
+      config.session_storage = @storage
+    end
+
+    expect {
+      client.rambler
+    }.to raise_error(NoMethodError)
+  end
 end
