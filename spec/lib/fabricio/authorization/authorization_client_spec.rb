302db1d5dc24ac937ba7672102f5557af06bb7e2
Egor Tolstoy <eetolstoy@avito.ru>
2017-10-07 14:50:37 +0300

    Refactored tests for authorization_client

--- spec/lib/fabricio/authorization/authorization_client_spec.rb
+++ spec/lib/fabricio/authorization/authorization_client_spec.rb
@@ -1,63 +1,63 @@
 require 'rspec'
-require 'webmock/rspec'
 require 'fabricio/authorization/authorization_client'
 require 'fabricio/authorization/session'
 
 describe 'Authorization Client' do
 
+  TEST_EMAIL_KEY = 'TEST_FABRICIO_EMAIL'
+  TEST_PASSWORD_KEY = 'TEST_FABRICIO_PASSWORD'
+  TEST_CLIENT_ID = 'TEST_CLIENT_ID'
+  TEST_CLIENT_SECRET = 'TEST_CLIENT_SECRET'
+
   TEST_STRING = 'string'
   TEST_TOKEN = 'token'
-  TEST_NETWORK_TOKEN = 'network_token'
   TEST_ORGANIZATION_ID = 'org_id'
   TEST_NETWORK_ORGANIZATION_ID = 'network_org_id'
 
   before(:each) do
     @client = Fabricio::Authorization::AuthorizationClient.new
-    @test_session = Fabricio::Authorization::Session.new({
-                                                          'access_token' => TEST_TOKEN,
-                                                          'refresh_token' => TEST_TOKEN
-                                                         }, TEST_ORGANIZATION_ID)
+    @test_session = @client.auth(
+               ENV[TEST_EMAIL_KEY],
+               ENV[TEST_PASSWORD_KEY],
+               ENV[TEST_CLIENT_ID],
+               ENV[TEST_CLIENT_SECRET]
+    )
   end
 
   it 'should return session for successful authorization' do
-    response_file = File.new(Dir.getwd + '/spec/lib/fabricio/authorization/authorization_success_stub_response.txt')
-    stub_request(:post, /token/).to_return(:body => response_file, :status => 200)
-    response_file = File.new(Dir.getwd + '/spec/lib/fabricio/authorization/organization_stub_response.txt')
-    stub_request(:get, /organizations/).to_return(:body => response_file, :status => 200)
-
-    session = @client.auth(TEST_STRING, TEST_STRING, TEST_STRING, TEST_STRING)
-
-    expect(session.access_token).to eq(TEST_NETWORK_TOKEN)
-    expect(session.refresh_token).to eq(TEST_NETWORK_TOKEN)
-    expect(session.organization_id).to eq(TEST_NETWORK_ORGANIZATION_ID)
+    expect(@test_session.access_token).not_to be_nil
+    expect(@test_session.refresh_token).not_to be_nil
+    expect(@test_session.organization_id).not_to be_nil
   end
 
   it 'should throw error on incorrect authorization response' do
-    response_file = File.new(Dir.getwd + '/spec/lib/fabricio/authorization/authorization_failure_stub_response.txt')
-    stub_request(:post, /token/).to_return(:body => response_file, :status => 401)
+    other_client = Fabricio::Authorization::AuthorizationClient.new
 
     expect {
-      @client.auth(TEST_STRING, TEST_STRING, TEST_STRING, TEST_STRING)
+      other_client.auth(
+          ENV[TEST_EMAIL_KEY],
+          'other_pass',
+          '2c18f8a77609ee6bbac9e53f3768fedc45fb96be0dbcb41defa706dc57d9c931',
+          '092ed1cdde336647b13d44178932cba10911577faf0eda894896188a7d900cc9'
+      )
     }.to raise_error(StandardError)
   end
 
   it 'should return session for successful refresh' do
-    response_file = File.new(Dir.getwd + '/spec/lib/fabricio/authorization/authorization_success_stub_response.txt')
-    stub_request(:post, /token/).to_return(:body => response_file, :status => 200)
-
     session = @client.refresh(@test_session)
 
-    expect(session.access_token).to eq(TEST_NETWORK_TOKEN)
-    expect(session.refresh_token).to eq(TEST_NETWORK_TOKEN)
-    expect(session.organization_id).to eq(TEST_ORGANIZATION_ID)
+    expect(session.access_token).not_to be_nil
+    expect(session.refresh_token).not_to be_nil
+    expect(session.organization_id).not_to be_nil
   end
 
   it 'should throw error on incorrect refresh response' do
-    response_file = File.new(Dir.getwd + '/spec/lib/fabricio/authorization/authorization_failure_stub_response.txt')
-    stub_request(:post, /token/).to_return(:body => response_file, :status => 401)
-
+    wrong_session = Fabricio::Authorization::Session.new({
+                                                             'access_token' => TEST_TOKEN,
+                                                             'refresh_token' => TEST_TOKEN
+                                                         }, TEST_ORGANIZATION_ID)
     expect {
-      @client.refresh(@test_session)
+      @client.refresh(wrong_session)
     }.to raise_error(StandardError)
   end
 end
