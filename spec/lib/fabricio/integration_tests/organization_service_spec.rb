8863ad3422a137a1752206cbfff3027e5f0e15c9
Egor Tolstoy <eetolstoy@avito.ru>
2017-10-07 16:16:46 +0300

    Refactored organization_service_tests

--- spec/lib/fabricio/integration_tests/organization_service_spec.rb
+++ spec/lib/fabricio/integration_tests/organization_service_spec.rb
@@ -1,27 +1,28 @@
 require 'rspec'
-require 'webmock/rspec'
+require_relative 'constants'
 require 'fabricio/services/organization_service'
 require 'fabricio/authorization/memory_session_storage'
 
 describe 'OrganizationService' do
 
   before(:each) do
+    auth_client = Fabricio::Authorization::AuthorizationClient.new
+    session = auth_client.auth(
+        ENV[TEST_EMAIL_KEY],
+        ENV[TEST_PASSWORD_KEY],
+        ENV[TEST_CLIENT_ID],
+        ENV[TEST_CLIENT_SECRET]
+    )
     storage = Fabricio::Authorization::MemorySessionStorage.new
-    session = Fabricio::Authorization::Session.new({
-                                                       'access_token' => '123',
-                                                       'refresh_token' => '123'
-                                                   }, '123')
     storage.store_session(session)
     client = Fabricio::Networking::NetworkClient.new(nil, storage)
-    @service = Fabricio::Service::OrganizationService.new(Fabricio::Authorization::Session.new, client)
+    @service = Fabricio::Service::OrganizationService.new(session, client)
   end
 
   it 'should fetch organization' do
-    response_file = File.new(Dir.getwd + '/spec/lib/fabricio/service/organization_service_get_stub_response.txt')
-    stub_request(:get, /organizations/).to_return(:body => response_file, :status => 200)
-
     result = @service.get
-    expect(result).not_to be_nil
+    expect(result.id).to eq ENV[TEST_ORGANIZATION_ID]
+    expect(result.json).not_to be_nil
   end
 
 end
